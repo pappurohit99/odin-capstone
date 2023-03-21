be1e2c43f9f6a1bb369aa333344eb57f0f06faf4
pappurohit99 <rohitpappu23@gmail.com>
2023-03-21 11:10:44 +0530

    Implemented numeric string evaluator

--- calculator/script.js
+++ calculator/script.js
@@ -1,22 +1,42 @@
+/* eslint-disable space-before-function-paren */
 /* Operations */
-
-/**
-    @param {number} a
-    @param {number} b
-    @returns {number} sum of elements
- */
-function add (a, b) {
+function add(a, b) {
     return a + b;
 }
 
-function subtract (a, b) {
+function subtract(a, b) {
     return a - b;
 }
 
-function multiply (a, b) {
-    return arr.reduce((prev, curr) => prev * curr, 1);
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide (a, b) {
+function divide(a, b) {
     return a / b;
 }
+
+/* Parsers */
+
+function operation(/** @type {string} */ numStr) {
+    let [leftOperand, rightOperand] = numStr.split(/[+\-*/]/g);
+    const rightOperandIdx = numStr.indexOf(rightOperand);
+    const operator = numStr[rightOperandIdx - 1];
+
+    leftOperand = Number(leftOperand.trim());
+    rightOperand = Number(rightOperand.trim());
+    let result = NaN;
+    switch (operator) {
+        case '+': result = add(leftOperand, rightOperand); break;
+        case '-': result = subtract(leftOperand, rightOperand); break;
+        case '*': result = multiply(leftOperand, rightOperand); break;
+        case '/': result = divide(leftOperand, rightOperand); break;
+    }
+    return result;
+}
+
+console.log(operation('23+45'));
+console.log(operation('45-13'));
+console.log(operation('12-24'));
+console.log(operation('1*0'));
+console.log(operation('1/0'));
