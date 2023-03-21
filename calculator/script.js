39bc708a250ef164f183e6b327eeb7fcd28d1a70
pappurohit99 <rohitpappu23@gmail.com>
2023-03-21 15:40:57 +0530

    Wired JS to UI

--- calculator/script.js
+++ calculator/script.js
@@ -1,4 +1,8 @@
 /* eslint-disable space-before-function-paren */
+
+// CONSTANTS
+const OPERATORS = ['+', '-', '*', '/'];
+
 /* Operations */
 function add(a, b) {
     return a + b;
@@ -16,15 +20,10 @@ function divide(a, b) {
     return a / b;
 }
 
-/* Parsers */
+/* Core functionality */
 
 function operation(/** @type {string} */ numStr) {
-    let [leftOperand, rightOperand] = numStr.split(/[+\-*/]/g);
-    const rightOperandIdx = numStr.indexOf(rightOperand);
-    const operator = numStr[rightOperandIdx - 1];
-
-    leftOperand = Number(leftOperand.trim());
-    rightOperand = Number(rightOperand.trim());
+    const [leftOperand, operator, rightOperand] = parseNumericExpression(numStr);
     let result = NaN;
     switch (operator) {
         case '+': result = add(leftOperand, rightOperand); break;
@@ -35,8 +34,59 @@ function operation(/** @type {string} */ numStr) {
     return result;
 }
 
-console.log(operation('23+45'));
-console.log(operation('45-13'));
-console.log(operation('12-24'));
-console.log(operation('1*0'));
-console.log(operation('1/0'));
+function updateResult (str) {
+    const resultDiv = document.querySelector('.results');
+    resultDiv.innerText = str;
+}
+
+/* Parsers and Validators */
+
+function parseNumericExpression(numStr) {
+    let [leftOperand, rightOperand] = numStr.split(/[+\-*/]/g);
+    const rightOperandIdx = numStr.indexOf(rightOperand);
+    const operator = numStr[rightOperandIdx - 1];
+
+    leftOperand = Number(leftOperand.trim());
+    rightOperand = Number(rightOperand.trim());
+    return [leftOperand, operator, rightOperand];
+}
+
+function isValidNumericalExpression(/** @type {string} */ str) {
+    // Scenario 1: string should have at least 3 characters
+    if (str.length < 3) {
+        return false;
+    }
+    if (OPERATORS.includes(str[0]) || OPERATORS.includes(str[str.length - 1])) {
+        return false;
+    }
+    const nOperators = str.match(/[+\-*/]/g);
+    if (nOperators === null) {
+        return false;
+    }
+    if (nOperators.length > 1) {
+        return false;
+    }
+    return true;
+}
+
+/* Event Handlers */
+
+function handleKeyClick(evt) {
+    const btnValue = this.innerText;
+    const resultDiv = document.querySelector('.results');
+    const resultStr = resultDiv.innerText;
+    if (btnValue === '=') {
+        if (!isValidNumericalExpression(resultStr)) {
+            return;
+        }
+        const ans = operation(resultStr);
+        updateResult(ans);
+    } else if (btnValue === 'AC') {
+        updateResult('');
+    } else if (!(OPERATORS.includes(btnValue) && OPERATORS.some(op => resultStr.includes(op)))) {
+        // Update the result string with button value
+        updateResult(resultStr === 'Infinity' ? btnValue : (resultStr + btnValue));
+    }
+}
+const buttons = document.querySelectorAll('.button');
+buttons.forEach(button => button.addEventListener('click', handleKeyClick));
