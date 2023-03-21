f71cc53d499a56f059195971a8c6f9a81ea9838a
Pappu Rohit <rohitpappu23@gmail.com>
2023-03-21 09:24:36 +0530

    Implemented basic operations

---
+++ calculator/script.js
@@ -0,0 +1,22 @@
+/* Operations */
+
+/**
+    @param {number} a
+    @param {number} b
+    @returns {number} sum of elements
+ */
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return arr.reduce((prev, curr) => prev * curr, 1);
+}
+
+function divide (a, b) {
+    return a / b;
+}
